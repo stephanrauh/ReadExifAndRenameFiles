0870ed89a5c6d7e712816466666062099c6ed5f2
stephan <webmaster1@beyondjava.de>
2019-10-17 21:13:36 +0200

    added *.heic support

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 const fs = require('fs');
 const openstreetmap = require('./openstreetmap');
 
-const dir = '(your image folder)'; // TODO put your image folder here
+const dir = '/Users/stephan/Dropbox/Photos/2019/Frankreich und Spanien'; // TODO put your image folder here
 
-async function renameFolder(folder) {
-  if (!folder.includes('.DS_Store')) {
-    const files = fs.readdirSync(folder);
+let folder = [
+  '2019-09-14 Autun abends',
+  '2019-09-15 Autun',
+  '2019-09-16 Cluny',
+  '2019-09-16 Le Puy en Velay Lumiere',
+  '2019-09-17 Le Puy en Velay und Umgebung',
+  '2019-09-18 Castelnau-Pégayrols und Millau',
+  '2019-09-19 Cité de Pierres',
+  '2019-09-19 Millau nachts',
+  '2019-09-20 Col d´Aspin Lourdes und Laruns',
+  '2019-09-21 Parc National de Pyrenees',
+  '2019-09-22 Pamplona',
+  '2019-09-23 Navarra',
+  '2019-09-24 Olite und Parque Natural de Bardenas',
+  '2019-09-25 Ordesa',
+  '2019-09-26 San Juan de la Peña',
+  '2019-09-27 Santuario',
+  '2019-09-28 Aínsa',
+  '2019-09-29 Parque Natural de la Sierra y los Cañones de Guara',
+  '2019-09-30 Geoparque de Sobrarbe',
+  '2019-10-01 Vielha und katalanische Pyrenäen',
+  '2019-10-02 Ainsa und Bielsa',
+  '2019-10-02 Dune de Pylat',
+  '2019-10-03 Bordeaux'
+];
+
+async function renameFolder(folderOrFile) {
+  if (fs.statSync(folderOrFile).isFile()) {
+    await openstreetmap.addInfoToFilename(folderOrFile);
+  } else if (!folder.includes('.DS_Store')) {
+    const files = fs.readdirSync(folderOrFile);
     for (var file of files) {
-      await openstreetmap.addInfoToFilename(folder + '/' + file);
+      await openstreetmap.addInfoToFilename(folderOrFile + '/' + file);
     }
   }
 }
 
-async function renameBatch() {
+async function renameBatch(f) {
   const folders = fs.readdirSync(dir);
   for (folder of folders) {
-    await renameFolder(dir + '/' + folder);
+    if (!folder.startsWith('.'))  {
+      await renameFolder(dir + '/' + folder);
+    }
   }
 }
 
--- openstreetmap.js
+++ openstreetmap.js
@@ -2,6 +2,79 @@ const https = require('https');
 const convert = require('xml-js');
 const fs = require('fs');
 const exif = require('exif-parser');
+const heicExif = require('./heic-exif-reader');
+
+async function parseJpgFile(file = 'some.jpg') {
+  const buffer = fs.readFileSync(file);
+  const parser = exif.create(buffer);
+  const result = parser.parse();
+  const hdr = result.tags.CustomRendered === 3;
+  const focalLength = result.tags.FocalLengthIn35mmFormat + 'mm';
+  await sleep(1100);
+  let newFile = file.substring(0, file.length - 4);
+  newFile += ' ' + focalLength;
+  if (hdr) {
+    newFile = newFile + ' hdr';
+  }
+  newFile = newFile + '.jpg';
+
+  // console.log(3);
+  if (result.tags.GPSLatitude && result.tags.GPSLongitude) {
+    // console.log(4);
+    getTown(result.tags.GPSLatitude, result.tags.GPSLongitude, result => {
+      newFile = newFile.substring(0, newFile.length - 4);
+      if (result) {
+        newFile = newFile + ' ' + result;
+      }
+      newFile = newFile + '.jpg';
+      console.log('uncomment line 85 to rename ' + newFile);
+      // fs.renameSync(file, newFile);
+    });
+  } else {
+    console.log('rename ' + newFile);
+    fs.renameSync(file, newFile);
+  }
+}
+
+async function parseHeicFile(file = 'some.heic') {
+  const buffer = fs.readFileSync(file);
+  const result = heicExif.findEXIFinHEIC(buffer);
+
+  //const hdr = result.tags.CustomRendered === 3;
+  const focalLength = result.FocalLengthIn35mmFilm + 'mm';
+  await sleep(1100);
+  let newFile = file.substring(0, file.length - 5);
+  const offset = newFile.lastIndexOf("/");
+  newFile = newFile.substring(0, offset+11) +  newFile.substring(offset+11, offset+19).replace("-", ".").replace("-", ".") + newFile.substring(offset+19);
+  newFile += ' ' + focalLength;
+  newFile = newFile + '.heic';
+
+  // console.log(3);
+  if (result.GPSLatitude && result.GPSLongitude) {
+    // console.log(4);
+    let long = result.GPSLongitude[0] + result.GPSLongitude[1]/60 + result.GPSLongitude[2]/3600;
+    if (result.GPSLongitudeRef === 'S') {
+      long = -long;
+    }
+    let lat = result.GPSLatitude[0] + result.GPSLatitude[1]/60 + result.GPSLatitude[2]/3600;
+    if (result.GPSLongitudeRef === 'W') {
+      long = -long;
+    }
+  
+    getTown(lat, long, result => {
+      newFile = newFile.substring(0, newFile.length - 5);
+      if (result) {
+        newFile = newFile + ' ' + result;
+      }
+      newFile = newFile + '.heic';
+      console.log('uncomment line 85 to rename ' + newFile);
+      fs.renameSync(file, newFile);
+    });
+  } else if (file !== newFile) {
+    console.log('rename ' + newFile);
+//    fs.renameSync(file, newFile);
+  }
+}
 
 const logTown = result => {
   console.log('Town = ' + result);
@@ -22,7 +95,7 @@ function getTown(latitude, longitude, callback = logTown) {
     }
   };
 
-  console.log(options.path);
+  // console.log(options.path);
 
   var g = https.get(
     options,
@@ -37,13 +110,15 @@ function getTown(latitude, longitude, callback = logTown) {
         const result1 = convert.xml2json(body, { compact: true, spaces: 4 });
         const geo = JSON.parse(result1);
         if (geo.reversegeocode.addressparts.town) {
-          callback(geo.reversegeocode.addressparts.town._text);
+          callback(geo.reversegeocode.addressparts.town._text.replace("/", "-"));
         } else if (geo.reversegeocode.addressparts.city) {
-          callback(geo.reversegeocode.addressparts.city._text);
+          callback(geo.reversegeocode.addressparts.city._text.replace("/", "-"));
         } else if (geo.reversegeocode.addressparts.village) {
-          callback(geo.reversegeocode.addressparts.village._text);
+          callback(geo.reversegeocode.addressparts.village._text.replace("/", "-"));
         } else if (geo.reversegeocode.addressparts.hamlet) {
-          callback(geo.reversegeocode.addressparts.hamlet._text);
+          callback(geo.reversegeocode.addressparts.hamlet._text.replace("/", "-"));
+        } else if (geo.reversegeocode.addressparts.city_district) {
+          callback(geo.reversegeocode.addressparts.city_district._text.replace("/", "-"));
         } else {
           console.log(geo.reversegeocode.addressparts);
         }
@@ -55,37 +130,17 @@ function getTown(latitude, longitude, callback = logTown) {
 }
 
 module.exports.addInfoToFilename = async function(file) {
+//  console.log(file);
   if (file.includes('mm')) {
     return;
   }
-  if (!file.endsWith('.jpg')) {
+  if (!file.endsWith('.heic')) {
     return;
   }
-  const buffer = fs.readFileSync(file);
-  const parser = exif.create(buffer);
-  const result = parser.parse();
-  const hdr = result.tags.CustomRendered === 3;
-  const focalLength = result.tags.FocalLengthIn35mmFormat + 'mm';
-  await sleep(1100);
-  let newFile = file.substring(0, file.length - 4);
-  newFile += ' ' + focalLength;
-  if (hdr) {
-    newFile = newFile + ' hdr';
-  }
-  newFile = newFile + '.jpg';
-
-  if (result.tags.GPSLatitude && result.tags.GPSLongitude) {
-    getTown(result.tags.GPSLatitude, result.tags.GPSLongitude, result => {
-      newFile = newFile.substring(0, newFile.length - 4);
-      if (result) {
-        newFile = newFile + ' ' + result;
-      }
-      newFile = newFile + '.jpg';
-      console.log('uncomment line 85 to rename ' + newFile);
-      // fs.renameSync(file, newFile);
-    });
+  // console.log('2');
+  if (file.endsWith('.heic')) {
+    parseHeicFile(file);
   } else {
-    console.log('rename ' + newFile);
-    fs.renameSync(file, newFile);
+    parseJpgFile(file);
   }
 };
